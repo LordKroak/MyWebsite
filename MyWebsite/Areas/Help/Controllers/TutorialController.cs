e2bfe1a8db45bb81a157909d39a44ac67d4661b1
Lord Kroak <jurassicgaming789@gmail.com>
2020-10-11 22:26:34 -0400

    Add project files.

---
+++ MyWebsite/Areas/Help/Controllers/TutorialController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            if (id == "Page2")
+            {
+                return View("Page2"); //Views/Tutorial/Page2.cshtml
+            }
+            if (id == "Page3")
+            {
+                return View("Page3"); //Views/Tutorial/Page3.cshtml
+            }
+            return View("Page1"); //Views/Tutorial/Page1.cshtml
+            //if anything other then 2 or 3 is put in, defaults to Page1
+        }
+    }
+}
---
+++ MyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace MyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(); // returns Views/Home/Index.cshtml
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            
+            return View(); // returns Views/Home/About.cshtml
+        }
+
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View(); //returns Views/Home/Contract.cshtml
+        }
+    }
+}
